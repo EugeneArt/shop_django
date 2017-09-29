87529b9acf8323550a892156588a220d4e4d757b
aegean <mailbox.aea@gmail.com>
2017-09-29 15:44:20 +0300

    fix breadcrumbs and fix url path to product in home page

--- shop/products/urls.py
+++ shop/products/urls.py
@@ -3,7 +3,7 @@ from products.views import ProductList, ProductDetail, CategoryProductList, Subc
 
 
 urlpatterns = [
-    url(r'^product/(?P<pk>\w+)/$', ProductDetail.as_view(), name='product'),
+    url(r'^products/(?P<product_category>\w+)/(?P<product_subcategory>\w+)/(?P<pk>\w+)/$', ProductDetail.as_view(), name='product'),
     url(r'^products/$', ProductList.as_view(), name='products'),
     url(r'^products/(?P<product_category>\w+)/$', CategoryProductList.as_view(), name='products_category'),
     url(r'^products/(?P<product_category>\w+)/(?P<product_subcategory>\w+)/$', SubcategoryProductList.as_view(), name='products_subcategory'),
--- shop/products/views.py
+++ shop/products/views.py
@@ -1,6 +1,17 @@
 from django.views.generic import ListView, DetailView
 from products.models import Product, ProductImage
 
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'products/product.html'
+    context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetail, self).get_context_data(**kwargs)
+        context['category'] = self.kwargs['product_category']
+        context['title'] = self.kwargs['product_subcategory']
+        return context
+
 class ProductList(ListView):
     model = ProductImage
     template_name = 'products/products.html'
@@ -12,15 +23,7 @@ class ProductList(ListView):
         context['title'] = 'Products'
         return context
 
-class ProductDetail(DetailView):
-    model = Product
-    template_name = 'products/product.html'
-    context_object_name = 'product'
-
-class CategoryProductList(ListView):
-    template_name = 'products/products.html'
-    context_object_name = 'products'
-    paginate_by = 6
+class CategoryProductList(ProductList):
 
     def get_queryset(self):
         products = ProductImage.objects.filter(product__subcategory__category__category_name=self.kwargs['product_category'],
@@ -32,10 +35,7 @@ class CategoryProductList(ListView):
         context['title'] = self.kwargs['product_category']
         return context
 
-class SubcategoryProductList(ListView):
-    template_name = 'products/products.html'
-    context_object_name = 'products'
-    paginate_by = 6
+class SubcategoryProductList(ProductList):
 
     def get_queryset(self):
         products = ProductImage.objects.filter(product__subcategory__category__category_name=self.kwargs['product_category'],
